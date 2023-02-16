80dd61856d6b6c8865af4967625787299a5bf57d
solomon244 <slegesse2444@gmail.com>
2023-02-17 02:43:30 +0300

    fix rotate 2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
-"""
- 0-rotate_2d_matrix
+"""0-rotate_2d_matrix
 """
 
 
 def rotate_2d_matrix(matrix):
-    """Rotate it 90 degrees clockwise
+    """Rotate it 90 degrees clockwise.
     """
-    m = len(matrix)
-    copy_matrix = [row[:] for row in matrix]
-    for i in range(0, n):
-        for j in range(m - 1, -1, -1):
-            matrix[j][m - 1 - i] = copy_matrix[i][j]
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
